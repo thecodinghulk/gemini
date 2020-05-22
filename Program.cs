6c93a99dd2355763d87587b06bf14cbeeea38ddf
kevin <kevin@MacBookAir.local>
2020-05-21 20:06:45 -0700

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gemini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gemini Exchange REST API Client");
+        }
+    }
+}
